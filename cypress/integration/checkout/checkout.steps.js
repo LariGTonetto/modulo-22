60b8c07a778db24ab799f9619f83eb6109695810
Larissa Tonetto <98757817+LariTonetto@users.noreply.github.com>
2023-02-20 11:53:47 -0300

    Update checkout.steps.js

--- cypress/integration/checkout/checkout.steps.js
+++ cypress/integration/checkout/checkout.steps.js
@@ -1,19 +1,19 @@
 /// <reference types="cypress" />
 
-import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
+import { Dado, Quando, Então } from 'cypress-cucumber-preprocessor/steps'
 const { pedidosPage } = require('../../support/page_objects')
 const dadosProdutos = require('../fixtures/prodRequest.json')
 const dadosLogin = require('../fixtures/perfil.json')
 
-Given('I have added a product in the cart', () => {
+Dado('Eu Adicione um produto no carrinho', () => {
     cy.addProduct(dadosProdutos.size, dadosProdutos.color, dadosProdutos.quantity,
         dadosProdutos.add_cart, dadosProdutos.product_id, dadosProdutos.variation_id)
 })
 
-When('I finish the order', () => {
+Quando('Eu termino o pedido', () => {
     cy.placeOrder(dadosLogin.usuario, dadosLogin.senha)
 })
 
-Then('I should see a message of success', () => {
+Então('Devo ver uma mensagem de sucesso', () => {
     pedidosPage.mensagem.should('contain', 'Obrigado. Seu pedido foi recebido.')
 })
