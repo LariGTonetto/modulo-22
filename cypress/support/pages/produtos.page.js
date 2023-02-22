b898f82b28caeb7230e8f1eadae601b3d4238b31
Larissa Tonetto <98757817+LariTonetto@users.noreply.github.com>
2023-02-22 11:15:33 -0300

    Update produtos.page.js

--- cypress/support/pages/produtos.page.js
+++ cypress/support/pages/produtos.page.js
@@ -6,21 +6,16 @@ class produtosPage {
         cy.get('.button-variable-item-' + tamanho).click()
         cy.get('.button-variable-item-' + cor).click()
         cy.get('.input-text').clear().type(quantidade)        
-        cy.get('.single_add_to_cart_button').click()
-    }
+        cy.get('.single_add_to_cart_button').click()}
 
     clicarBotaoCarrinho(){
-        cy.get('.woocommerce-message > .button').click()
-    }
+        cy.get('.woocommerce-message > .button').click()}
 
     clicarPreviewCarrinho(){
-        cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()
-    }
+        cy.get('.dropdown-toggle > .text-skin > .icon-basket').click()}
 
     get PreviewCarrinho(){
-        return cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .cart_list')
-    }
-
+        return cy.get('#cart > .dropdown-menu > .widget_shopping_cart_content > .mini_cart_content > .mini_cart_inner > .mcart-border > .cart_list')}
 
 }
 
